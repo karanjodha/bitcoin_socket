74e2b0c2640b8af08bce7b52ddd33b256e451739
Karan Jodha <karanjodha97@gmail.com>
2019-06-21 20:26:44 +0530

    fix

--- server.js
+++ server.js
@@ -4,17 +4,15 @@ var request = require('request');
 setInterval(() => {
     // willexecute every second
     
-    const HASH='ec21ad7a1156c253c00cb36c48d943afc8179be8beddf4c5edaf965bcb1e4ad4'
-    const URL='https://insight.bitpay.com/api/tx/'+HASH
-    
-
+    const HASH='1D9o3c45VkQHDVCXGtJ42z29NVM6TycL1c'
+    const URL='https://insight.bitpay.com/api/txs?address='+HASH+'&pageNum=0'
 
     request(URL, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body)  
         }
         else{
-            console.log('Something Fishy...'+err)
+            console.log('Something Fishy...'+error)
         }
     })
-}, 1000);
+}, 2000);
